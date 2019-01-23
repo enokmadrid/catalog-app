173bfc889da6a67c46664e2be254ff7708028298
Enok Madrid <enokslaboratory@gmail.com>
2019-01-22 18:00:32 -0800

    genrate default auth with passport

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/developer', function () {
+    return view('developer');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

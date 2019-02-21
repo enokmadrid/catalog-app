4365f30038b8a2e7b325bfb4b0c8bd827f1008ae
Enok Madrid <enokslaboratory@gmail.com>
2019-02-20 19:45:17 -0800

    Update 2019_02_19_192113_create_categories_table.php

--- database/migrations/2019_02_19_192113_create_categories_table.php
+++ database/migrations/2019_02_19_192113_create_categories_table.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Migrations\Migration;
 class CreateCategoriesTable extends Migration
 {
     /**
+     * Run the migrations.
+     *
      * @return void
      */
     public function up()
